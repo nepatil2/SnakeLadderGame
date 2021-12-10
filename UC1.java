f5d8f6485723ba83551770e51f7c2d5a2c657b2a
nepatil2 <nehapatil22899@gmail.com>
2021-12-10 12:52:55 +0530

    UC1 Snake Game Started from 0th Position

---
+++ UC1.java
@@ -0,0 +1,8 @@
+package SnakeLadder;
+
+public class UC1 {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("Player one started game played at position " + position);
+    }
+}
